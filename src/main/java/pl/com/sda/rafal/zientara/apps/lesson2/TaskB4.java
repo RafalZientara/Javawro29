9059fe3ef385c077c36cd47b226bf28c0836e1ef
Michal <uzytkownik@kursgit.pl>
2020-07-04 12:06:53 +0200

    no message

--- src/main/java/pl/com/sda/rafal/zientara/apps/lesson2/TaskB4.java
+++ src/main/java/pl/com/sda/rafal/zientara/apps/lesson2/TaskB4.java
@@ -1,4 +1,8 @@
 package pl.com.sda.rafal.zientara.apps.lesson2;
 
 public class TaskB4 {
+    public static void main(String[] args) {
+        System.out.println("Task B4");
+        System.out.println("--------");
+    }
 }
