2deea9279852af782d0efd0cb4edc2b6962d29d9
hrynowieckip <hrynowieckip@gmail.com>
2020-07-04 11:47:19 +0200

    Hi2

--- src/main/java/pl/com/sda/rafal/zientara/apps/Main.java
+++ src/main/java/pl/com/sda/rafal/zientara/apps/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Javawro29");
         System.out.println("hrynowieckip");
+        System.out.println("Cos");
 
         Singleton instance = Singleton.getInstance();
 
