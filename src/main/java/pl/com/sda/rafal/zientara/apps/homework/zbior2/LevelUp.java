82519c21d53bc9253c37a40e1569d84fa9e8bf14
Rafal Zientara <rafal.zientara92@gmail.com>
2020-07-20 19:02:32 +0200

    lista 2 zad 4 + mockito nasluchujace wywolanie funkcji

--- src/main/java/pl/com/sda/rafal/zientara/apps/homework/zbior2/LevelUp.java
+++ src/main/java/pl/com/sda/rafal/zientara/apps/homework/zbior2/LevelUp.java
@@ -2,6 +2,10 @@ package pl.com.sda.rafal.zientara.apps.homework.zbior2;
 
 public class LevelUp {
 
+    private int level;
+    private int experience;
+    private LevelListener listener;
+
     public int countExperienceRequired(int level) {
         int exp = 0;
         int required = 10;
@@ -12,4 +16,34 @@ public class LevelUp {
         return exp;
     }
 
+    public int getLevel() {
+        return level;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void addExp(int newExperience) {
+        experience += newExperience;
+        checkLevel();
+    }
+
+    private void checkLevel() {
+        while (countExperienceRequired(level + 1) <= experience) {
+            level++;
+            if (listener != null) {
+                listener.onLevelUp(level);
+            }
+        }
+    }
+
+    public void setListener(LevelListener listener) {
+        this.listener = listener;
+    }
+
+    public interface LevelListener {
+        void onLevelUp(int level);
+    }
 }
+
--- src/main/test/pl/com/sda/rafal/zientara/apps/homework/zbior2/LevelUpTest.java
+++ src/main/test/pl/com/sda/rafal/zientara/apps/homework/zbior2/LevelUpTest.java
@@ -2,8 +2,11 @@ package pl.com.sda.rafal.zientara.apps.homework.zbior2;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 
 class LevelUpTest {
 
@@ -21,5 +24,82 @@ class LevelUpTest {
         assertEquals(70, levelUp.countExperienceRequired(3));
         assertEquals(150, levelUp.countExperienceRequired(4));
     }
+
+    @Test
+    public void playerStartsWith0Level() {
+        assertEquals(0, levelUp.getLevel());
+        assertEquals(0, levelUp.getExperience());
+    }
+
+    @Test
+    public void playerGetsFirstLevel() {
+        levelUp.addExp(10);
+
+        assertEquals(1, levelUp.getLevel());
+        assertEquals(10, levelUp.getExperience());
+    }
+
+    @Test
+    public void playerGetsFirstLevelWithSomeExtraExperience() {
+        levelUp.addExp(29);
+
+        assertEquals(1, levelUp.getLevel());
+        assertEquals(29, levelUp.getExperience());
+    }
+
+    @Test
+    public void playerGetsTwoLevelsAtOnce() {
+        levelUp.addExp(30);
+
+        assertEquals(2, levelUp.getLevel());
+        assertEquals(30, levelUp.getExperience());
+    }
+
+    @Test
+    public void playerGetsThreeLevelsAtOnce() {
+        levelUp.addExp(72);
+
+        assertEquals(3, levelUp.getLevel());
+        assertEquals(72, levelUp.getExperience());
+    }
+
+    @Test
+    public void playerGetsLevelTwoAfterTwoAddedPoints() {
+        levelUp.addExp(10);
+        levelUp.addExp(20);
+
+        assertEquals(2, levelUp.getLevel());
+        assertEquals(30, levelUp.getExperience());
+    }
+
+    @Test
+    public void playerGetsLevelTwoAfterThreeAddedPoints() {
+        levelUp.addExp(10);
+        levelUp.addExp(10);
+        levelUp.addExp(10);
+
+        assertEquals(2, levelUp.getLevel());
+        assertEquals(30, levelUp.getExperience());
+    }
+
+    @Test
+    public void listenerCanHearLevelUp() {
+        LevelUp.LevelListener listener = Mockito.mock(LevelUp.LevelListener.class);
+        levelUp.setListener(listener);
+
+        levelUp.addExp(10);
+
+        Mockito.verify(listener, Mockito.times(1)).onLevelUp(1);
+    }
+    @Test
+    public void listenerCanHearEveryLevelUp() {
+        LevelUp.LevelListener listener = Mockito.mock(LevelUp.LevelListener.class);
+        levelUp.setListener(listener);
+
+        levelUp.addExp(150);
+
+        Mockito.verify(listener, Mockito.times(4)).onLevelUp(anyInt());
+    }
+
 
 }
